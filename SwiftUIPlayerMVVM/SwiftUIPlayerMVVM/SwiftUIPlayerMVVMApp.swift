094378ccad6eed6c3041d69e4f3c479e12763cde
xach55 <xach55@gmail.com>
2024-05-02 11:34:43 +0400

    initial commit

---
+++ SwiftUIPlayerMVVM/SwiftUIPlayerMVVM/SwiftUIPlayerMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPlayerMVVMApp.swift
+//  SwiftUIPlayerMVVM
+//
+//  Created by Levon Shaxbazyan on 02.05.24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPlayerMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
